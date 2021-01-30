fa6a1f5ed583957bf9a881d3a6983b1dc91a6a25
GH-Mina <you@example.com>
2021-01-30 22:50:52 +0100

    Add junit test

--- src/test/java/org/si/BanqueSiApplicationTests.java
+++ src/test/java/org/si/BanqueSiApplicationTests.java
@@ -3,12 +3,28 @@ package org.si;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
-class BanqueSiApplicationTests {
+class BankSiApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public  void assertThatOnePlusOneIsTwo(){
+		int somme =1+1;
+		assertThat(somme).isNotNull();
+		assertThat(somme).isEqualTo(2);
+	}
+	@Test
+	public  void assertThatOnePlusTwoIsThree(){
+		int somme =2+2;
+		assertThat(somme).isNotNull();
+		assertThat(somme).isEqualTo(4);
+	}
+
+
+
 
 }
